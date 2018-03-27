bbe17aebd5b40f9a65a3c440b24d922fd1c23c25
minhanbkdn <36067662+minhanbkdn@users.noreply.github.com>
2018-03-27 23:38:37 +0700

    v1.0

---
+++ viewpager/app/src/main/java/com/example/dong/devpro_viewpager/PagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.dong.devpro_viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+public class PagerAdapter  extends FragmentStatePagerAdapter {
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment frag=null;
+        switch (position){
+            case 0:
+                frag=new AndroidFragment();
+                break;
+            case 1:
+                frag=new IOSFragment();
+                break;
+            case 2:
+                frag=new PHPFragment();
+                break;
+        }
+        return frag;
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title="Android";
+                break;
+            case 1:
+                title="IOS";
+                break;
+            case 2:
+                title="PHP";
+                break;
+        }
+
+        return title;
+    }
+
+}
